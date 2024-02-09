4feef5706ab2c7c7425ea6c192d978d44f2771cb
Raofin <zaidaminraofin@gmail.com>
2024-02-09 23:51:56 +0600

    Add initial layout

---
+++ src/OSL.WEB/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OSL.WEB.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
